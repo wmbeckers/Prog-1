cdd4b743ff0c9a96e3d29f9d5fc6fe8f9a9d5942
Murilo <64659295+wmbeckers@users.noreply.github.com>
2024-08-08 10:28:23 -0300

    Add files via upload
    calculadora simples em python

---
+++ teste.py
@@ -0,0 +1,32 @@
+loop = 1
+oper = 0
+
+while loop == 1:
+
+    x = input("Pressione 9 para cancelar a operação.\nCaso deseje continuar, pressione qualquer outra tecla\n")
+    if x == "9":
+        break
+
+    num1 = input("Selecione o primeiro número: ")
+    print (num1) 
+    
+    oper = input("Selecione o operador:\n+: para adição\n-: para subtração\n*: para multiplicação\n/: para divisão.\n")
+    print (oper)
+
+    num2 = input("Selecione o segundo número: ")
+    print (num2)
+    
+    if oper == "+":
+        result = float(num1) + float(num2)
+    elif oper == "-":
+        result = float(num1) - float(num2)
+    elif oper == "*":
+        result = float(num1) * float(num2)
+    elif oper == "/":
+        result = float(num1) / float(num2)
+    else:
+        print("Operador inválido!")
+
+    print ("O resultado de " + str(num1) + str(oper) + str(num2) + " é " + str(result))
+
+    print("\nPressione 9 para sair.")
