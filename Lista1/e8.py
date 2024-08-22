36b921fe5c873c11181a29e5d9a43402cace75b6
Murilo <64659295+wmbeckers@users.noreply.github.com>
2024-08-22 11:02:34 -0300

    Create e8.py

---
+++ Lista1/e8.py
@@ -0,0 +1,12 @@
+n1 = input('Insira o primeiro número\n')
+n2 = input('Insira o segundo número\n')
+n3 = input('Insira o terceiro número\n')
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
+add = n1+n2+n3
+med = (n1+n2+n3)/3
+pro = n1*n2*n3
+min = min(n1, n2, n3)
+max = max(n1, n2, n3)
+print('A soma desses números é:', add, '\nA média desses números é:', med, '\nO produto desses números é:', pro, '\nO menor desses números é:', min, '\nE o maior desses números é:', max)
