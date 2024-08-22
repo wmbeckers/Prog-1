26f9a0e9758015af2a83286e5f289e0e31afbaf0
Murilo <64659295+wmbeckers@users.noreply.github.com>
2024-08-22 10:38:58 -0300

    Rename e5.py to Lista1/e5.py

--- e5.py
+++
@@ -1,8 +0,0 @@
-op1 = 27.5 + 2
-op2 = 27.5 - 2 
-op3 = 27.5 * 2
-op4 = 27.5 / 2
-op5 = 27.5 ** 2
-op6 = 27.5 // 2
-
-print('Os resultados das operações são:\n', op1, '\n', op2, '\n', op3, '\n', op4, '\n', op5)
