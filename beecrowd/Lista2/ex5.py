c86c3b817d371280138abd17f8d78044320a9cb0
Murilo <64659295+wmbeckers@users.noreply.github.com>
2024-09-04 13:41:48 -0300

    Update ex5.py

--- beecrowd/Lista2/ex5.py
+++ beecrowd/Lista2/ex5.py
@@ -1,16 +1,19 @@
-notas = list(map(float, input().split()))
-media = (notas[0] * 2 + notas[1] * 3 + notas[2] * 4 + notas[3] * 1) / 10
-print("Media:", format(media, ".1f"))
+N1, N2, N3, N4 = map(float, input().split())
+
+media = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10
+print(f"Media: {media:.1f}")
+
 if media >= 7.0:
     print("Aluno aprovado.")
 elif media < 5.0:
     print("Aluno reprovado.")
 else:
     print("Aluno em exame.")
-    nota_exame = float(input("Nota do exame: "))
-    media_final = (media + nota_exame) / 2
-    print("Media final:", format(media_final, ".1f"))
+    exame = float(input())
+    print(f"Nota do exame: {exame:.1f}")
+    media_final = (media + exame) / 2
     if media_final >= 5.0:
         print("Aluno aprovado.")
     else:
         print("Aluno reprovado.")
+    print(f"Media final: {media_final:.1f}")
