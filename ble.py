34241b2d15a78cbbc2f1e75a46a91cef9f62170e
Murilo <64659295+wmbeckers@users.noreply.github.com>
2024-10-12 15:10:47 -0300

    Update ble.py

--- ble.py
+++ ble.py
@@ -4,5 +4,3 @@ sg = input('Indique os segundos\n')
 
 ssm = ((int(hr)*60+int(mn))*60)+int (sg)
 print('fazem ', ssm, ' seg desde a meia noite')
-
-## https://stackoverflow.com/questions/19821273/limiting-user-input-to-a-range-in-python
