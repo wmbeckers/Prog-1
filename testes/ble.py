0b850cbfa0598aef394a91dd4708171591f97051
Murilo <64659295+wmbeckers@users.noreply.github.com>
2024-11-05 21:23:17 -0300

    tstes

--- testes/ble.py
+++
@@ -1,6 +0,0 @@
-hr = input('Indique a hora\n')
-mn = input('Indique os minutos\n')
-sg = input('Indique os segundos\n')
-
-ssm = ((int(hr)*60+int(mn))*60)+int (sg)
-print('fazem ', ssm, ' seg desde a meia noite')
