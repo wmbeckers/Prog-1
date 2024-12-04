62473e098e1c3d8cefcc2bc73e5ceaa2d16d7077
Murilo <64659295+wmbeckers@users.noreply.github.com>
2024-12-04 12:48:48 -0300

    Update ex4.py
    aceito pelo beecrowd

--- beecrowd/Lista4/ex4.py
+++ beecrowd/Lista4/ex4.py
@@ -1,22 +1,31 @@
-"""nao adicionado ao beecrowd, mas funciona perfeitamente"""
+def print_vector(name, vector):
+    for i in range(len(vector)):
+        print(f"{name}[{i}] = {vector[i]}")
 
-par = []
-impar = []
+def main():
+    data = []
+    for _ in range(15):
+        data.append(int(input().strip()))
 
-numeros = []
+    par = []
+    impar = []
 
-for _ in range(15):
-    num = int(input())
-    numeros.append(num)
+    for num in data:
+        if num % 2 == 0:
+            par.append(num)
+            if len(par) == 5:
+                print_vector("par", par)
+                par = []
+        else:
+            impar.append(num)
+            if len(impar) == 5:
+                print_vector("impar", impar)
+                impar = []
 
-for num in numeros:
-    if num % 2 == 0:
-        par.append(num)
-    else:
-        impar.append(num)
+    if impar:
+        print_vector("impar", impar)
+    if par:
+        print_vector("par", par)
 
-for i in range(len(par)):
-    print(f"par[{i}] = {par[i]}")
-
-for i in range(len(impar)):
-    print(f"impar[{i}] = {impar[i]}")
+if __name__ == "__main__":
+    main()
